b5cf799af5b3ef0084db3144d0dd2d3ae5675a92
iotarex <z_series@tuta.io>
2020-08-30 21:55:41 -0500

    WIP for fingerprinting class.

--- app/src/main/java/com/dangerousthings/nfc/utilities/FingerprintUtils.java
+++ app/src/main/java/com/dangerousthings/nfc/utilities/FingerprintUtils.java
@@ -1,7 +1,9 @@
 package com.dangerousthings.nfc.utilities;
 
 import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
 import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
 
 import com.dangerousthings.nfc.enums.TagType;
 import com.dangerousthings.nfc.models.FlexDF;
@@ -14,6 +16,7 @@ import com.dangerousthings.nfc.models.NExT;
 import com.dangerousthings.nfc.models.XDF2;
 import com.dangerousthings.nfc.models.XNT;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +25,8 @@ public class FingerprintUtils
     public static final int SIZE_NTAG_216 = 868;
     public static final int SIZE_DESFIRE_EV1_8K = 7676;
 
+    public static final String GET_VERSION = "60";
+
     public static TagType fingerprintNfcTag(Tag tag)
     {
         Ndef ndef = Ndef.get(tag);
@@ -29,11 +34,28 @@ public class FingerprintUtils
         switch(capacity)
         {
             case SIZE_NTAG_216:
+                //send GET_VERSION command
+                byte[] get_version = sendNfcHexCommand(GET_VERSION, tag);
                 return TagType.Ntag216;
             case SIZE_DESFIRE_EV1_8K:
                 return TagType.DesfireEv18k;
         }
         return null;
+    }
+
+    private static byte[] sendNfcHexCommand(String command, Tag tag)
+    {
+        NfcA nfcA = NfcA.get(tag);
+        byte[] commandBytes = HexUtils.hexToBytes(command);
+        try
+        {
+            nfcA.connect();
+            return nfcA.transceive(commandBytes);
+        }
+        catch(IOException e)
+        {
+            return null;
+        }
     }
 
     public static List<Implant> getImplantListFromType(TagType tagType)
--- app/src/main/java/com/dangerousthings/nfc/utilities/HexUtils.java
+++ app/src/main/java/com/dangerousthings/nfc/utilities/HexUtils.java
@@ -18,13 +18,13 @@ public class HexUtils
         }
     }
 
+    //hex must be an even length string
     public static byte[] hexToBytes(String hex) {
         hex = hex.replaceAll("\\s+","");
         int len = hex.length();
         byte[] bytes = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
-                                  + Character.digit(hex.charAt(i+1), 16));
+        for (int i = 0; i < len-1; i += 2) {
+            bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character.digit(hex.charAt(i+1), 16));
         }
         return bytes;
     }
