71cc890a13ed471ab061e9957bf20e2b1cc3e6d4
iotarex <z_series@tuta.io>
2021-06-07 00:11:42 -0500

    Changed how I call colors and added a new fourth color code to use in themes.

--- app/src/main/java/com/dangerousthings/nfc/utilities/ColorUtils.java
+++ app/src/main/java/com/dangerousthings/nfc/utilities/ColorUtils.java
@@ -40,16 +40,25 @@ public class ColorUtils
         return color;
     }
 
+    public static int getSecondaryColor(Context context)
+    {
+        TypedValue typedValue = new TypedValue();
+        TypedArray a = context.obtainStyledAttributes(typedValue.data, new int[] {R.attr.colorSecondary});
+        int color = a.getColor(0, 0);
+        a.recycle();
+        return color;
+    }
+
     public static List<Theme> getThemeList()
     {
         List<Theme> themes = new ArrayList<>();
-        themes.add(new Theme(R.style.DT, "DT", Color.parseColor("#FFFFFF"), Color.parseColor("#000000"), Color.parseColor("#F56528")));
-        themes.add(new Theme(R.style.DEOT, "DEOT", Color.parseColor("#008F78"), Color.parseColor("#0A1017"), Color.parseColor("#BBC6C5")));
-        themes.add(new Theme(R.style.OPZ, "OPZ", Color.parseColor("#BCBDC2"), Color.parseColor("#4B4E5B"), Color.parseColor("#E0B000")));
-        themes.add(new Theme(R.style.Wog, "Wog", Color.parseColor("#FFF6D8"), Color.parseColor("#167D7F"), Color.parseColor("#B5E5CF")));
-        themes.add(new Theme(R.style.BlackOnWhite, "Black on White", Color.parseColor("#FFFFFF"), Color.parseColor("#000000"), Color.parseColor("#7F7F7F")));
-        themes.add(new Theme(R.style.MurderedOut, "Murdered Out", Color.parseColor("#BEBEBE"), Color.parseColor("#000000"), Color.parseColor("#7F7F7F")));
-        themes.add(new Theme(R.style.Zytel, "Zytel", Color.parseColor("#F6B117"), Color.parseColor("#18433F"), Color.parseColor("#AA797B")));
+        themes.add(new Theme(R.style.DT, "DT", Color.parseColor("#FFFFFF"), Color.parseColor("#000000"), Color.parseColor("#FFFFFF"), Color.parseColor("#F56528")));
+        themes.add(new Theme(R.style.DEOT, "DEOT", Color.parseColor("#008F78"), Color.parseColor("#0A1017"), Color.parseColor("#008F78"), Color.parseColor("#BBC6C5")));
+        themes.add(new Theme(R.style.OPZ, "OPZ", Color.parseColor("#BCBDC2"), Color.parseColor("#4B4E5B"), Color.parseColor("#BCBDC2"), Color.parseColor("#E0B000")));
+        themes.add(new Theme(R.style.Wog, "Wog", Color.parseColor("#FFF6D8"), Color.parseColor("#167D7F"), Color.parseColor("#FFF6D8"), Color.parseColor("#B5E5CF")));
+        themes.add(new Theme(R.style.BlackOnWhite, "Black on White", Color.parseColor("#FFFFFF"), Color.parseColor("#000000"), Color.parseColor("#FFFFFF"), Color.parseColor("#7F7F7F")));
+        themes.add(new Theme(R.style.MurderedOut, "Murdered Out", Color.parseColor("#BEBEBE"), Color.parseColor("#000000"), Color.parseColor("#BEBEBE"), Color.parseColor("#7F7F7F")));
+        themes.add(new Theme(R.style.Zytel, "Zytel", Color.parseColor("#FBB518"), Color.parseColor("#143130"), Color.parseColor("#C08A8E"), Color.parseColor("#DFDCF0")));
         return themes;
     }
 }
